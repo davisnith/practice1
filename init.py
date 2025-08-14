9234e9181d64b65d113e7e8f09742afd9afc8385
davisnith <davisnith@gmail.com>
2025-08-14 16:59:56 +0530

    added code for sum of first five prime number

---
+++ init.py
@@ -0,0 +1,20 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+def average_first_n_primes(n):
+    primes = []
+    num = 2
+    while len(primes) < n:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return sum(primes) / n
+
+# Find average of first 5 prime numbers
+avg = average_first_n_primes(5)
+print("Average of first 5 prime numbers:", avg)
